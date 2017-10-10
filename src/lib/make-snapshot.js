84712acc53d1c55a49587f997d4579473a7f35a8
pjanik <janikpiotrek@gmail.com>
2017-10-10 12:50:36 +0400

    Handle / ignore navigation errors
    [#151654060]
    
    It might happen when there's e.g. iframe with a wrong url.
    It doesn't make sense to crash the whole process because of that.

--- src/lib/make-snapshot.js
+++ src/lib/make-snapshot.js
@@ -24,10 +24,17 @@ function getHtml (html = '', css = '', baseUrl = '') {
 module.exports = async function makesSnapshot (options, browser) {
   const page = await browser.newPage()
   await page.setViewport({width: options.width, height: options.height})
-  // .setContent could be more appropriate method, but it doesn't support waitUntil option.
-  // See: https://github.com/GoogleChrome/puppeteer/issues/728
-  await page.goto(options.url ? options.url : `data:text/html,${getHtml(options.html, options.css, options.baseUrl)}`,
-    {waitUntil: 'networkidle', networkIdleTimeout: 5000})
+  try {
+    // .setContent could be more appropriate method, but it doesn't support waitUntil option.
+    // See: https://github.com/GoogleChrome/puppeteer/issues/728
+    await page.goto(options.url ? options.url : `data:text/html,${getHtml(options.html, options.css, options.baseUrl)}`,
+      {waitUntil: 'networkidle', networkIdleTimeout: 5000})
+  } catch (e) {
+    // Sometimes .goto might fail when some resources can't be loaded (e.g. iframe with wrong url).
+    // This kind of error can be ignored.
+    console.warn('page.goto error (probably navigation error):')
+    console.warn(e)
+  }
   const screenshotPath = path.join('/tmp', `${new Date().getTime()}-${Math.round(Math.random() * 1e6)}.png`)
   await page.screenshot({path: screenshotPath})
   await page.close()
